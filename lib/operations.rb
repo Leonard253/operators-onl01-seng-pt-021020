3d53bd7ac32c025626c9f00f819b179863f1d568
Trey Williams <leonardwilliamsiii20@gmail.com>
2020-02-13 07:52:25 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,10 @@
 def unsafe?(speed)
     if speed > 60
-     return unsafe 
+     return "unsafe"
     elsif speed < 40
-    return unsafe
+    return "unsafe"
     else
-      return safe
+      return "safe"
 end
 
 
